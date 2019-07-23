ff17739aa66a48f6733002e36f62a2016ee16569
Evan Allen <evanallen13@gmail.com>
2019-07-23 16:57:09 -0700

    .

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import HeaderDiv from './components/headerComponents/header'
 import ProjectsPage from './components/projectsComponents/projectPage'
 import About from './components/AboutComponents/about'
+import Contact from './components/contact/contact'
 
 function App() {
   
@@ -11,6 +12,7 @@ function App() {
       <HeaderDiv></HeaderDiv>
       <ProjectsPage></ProjectsPage>
       <About></About>
+      <Contact></Contact>
     </div>
   );
 }
---
+++ src/components/contact/contact.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './contact.css'
+
+class Contact extends Component {
+    render() { 
+        return ( 
+            <div id="contact">
+                <h1>Contact</h1> 
+                <h2>Evan Allen</h2>
+                <div className="info">
+                    <h4>Phone: <a href="tel:714-264-2562">(714)264-2562</a></h4>
+                    <h4>Email: <a href="mailto:evanallen13@gmail.com">evanallen13@gmail.com</a></h4>
+                </div>
+            </div>
+         );
+    }
+}
+ 
+export default Contact;
