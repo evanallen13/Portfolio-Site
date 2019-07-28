d3f193a5c22f108c2385db3c521cc8aac190d0c4
Evan Allen <evanallen13@gmail.com>
2019-07-28 12:28:24 -0700

    add project overlay

--- src/components/projectsComponents/projects.jsx
+++ src/components/projectsComponents/projects.jsx
@@ -4,10 +4,11 @@ import './project.css'
 class Project extends Component {
     render() { 
         return ( 
-            <div className='p-2 overplayContainer'>
+            <div className='p-2'>
                 <h4>{this.props.Title}</h4>
-                <img src={this.props.Img} alt="project" className="projectBigImg"></img>
-                <div className="project-links">
+                <div className='overplayContainer'>
+                    <img src={this.props.Img} alt="project" className="projectBigImg"></img>
+                    <div className="project-links">
                     <a href={this.props.Github} target="_blank" rel="noopener noreferrer" >
                          <img src="../../imgs/projects/github.png" alt="git" className="projectImg"/>
                      </a>
@@ -15,13 +16,14 @@ class Project extends Component {
                          <img src="../../imgs/projects/eye.png" alt="git" className="projectImg"/>
                      </a>
                 </div>
+                </div>
                 <div className="experience">
-                     <h5>What I Learned:</h5>
-                     <p className="learned">{this.props.Learned}</p>
-                     <h5>Skills:</h5>
+                <h5>Skills:</h5>
                      {this.props.Skills.map(skill =>
                          <img src={skill} alt="skill" className='skill'></img>
                      )}
+                     <h5>What I Learned:</h5>
+                     <p className="learned">{this.props.Learned}</p>
                  </div>
             </div>
             // <div className="project">
