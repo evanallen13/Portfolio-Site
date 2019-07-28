8ae8e5bb9ce3f9f4c20a249edc52a90476299091
Evan Allen <evanallen13@gmail.com>
2019-07-28 12:36:17 -0700

    img size

--- src/components/projectsComponents/projects.jsx
+++ src/components/projectsComponents/projects.jsx
@@ -26,26 +26,6 @@ class Project extends Component {
                      <p className="learned">{this.props.Learned}</p>
                  </div>
             </div>
-            // <div className="project">
-            //     <h4>{this.props.Title}</h4>
-            //     <img src={this.props.Img} alt="project" className="projectBigImg"></img>
-            //     <div className="projectFooter">
-            //     <a href={this.props.Github} target="_blank" rel="noopener noreferrer" >
-            //         <img src="../../imgs/projects/github.png" alt="git" className="projectImg"/>
-            //     </a>
-            //     <a href={this.props.Hosting} target="_blank" rel="noopener noreferrer">
-            //         <img src="../../imgs/projects/eye.png" alt="git" className="projectImg"/>
-            //     </a>
-            //     </div>
-            //     <div className="experience">
-            //         <h5>Skills:</h5>
-            //         {this.props.Skills.map(skill =>
-            //             <img src={skill} alt="skill" className='skill'></img>
-            //         )}
-            //         <h5>What I Learned:</h5>
-            //         <p className="learned">{this.props.Learned}</p>
-            //     </div>
-            // </div>
         );
     }  
 }
